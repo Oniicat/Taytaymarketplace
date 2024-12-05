08aaacbccf280401caff194be71d5c3a421cd87e
Oniicat <calitisinmarkgil20@gmail.com>
2024-12-05 13:49:42 +0800

    Changed title of main.php sa admin

--- admin final na to/main.php
+++ admin final na to/main.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin</title>
+    <title>Admin ni lodi</title>
     <link rel="stylesheet" href="sidebarstyle.css">
 </head>
 <body>
