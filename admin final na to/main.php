ccc9e5b0785035576123245410f8f7587f82567d
Oniicat <calitisinmarkgil20@gmail.com>
2024-12-05 14:42:41 +0800

    changed title of main.php

--- admin final na to/main.php
+++ admin final na to/main.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Taytaymarketplace</title>
+    <title>Taytaymarketplace</title>
     <link rel="stylesheet" href="sidebarstyle.css">
 </head>
 <body>
