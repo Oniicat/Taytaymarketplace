af7d8650bbb396abb34e8d6f690c4e482584bd89
Jullian Miguel <jullianmiguel17@gmail.com>
2024-12-06 19:08:23 +0800

    change activityphp

--- admin final na to/activity_log.php
+++ admin final na to/activity_log.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Activity LOG</title>
+    <title>Activity LOGG</title>
     <style>
         body {
             font-family: Arial, sans-serif;
