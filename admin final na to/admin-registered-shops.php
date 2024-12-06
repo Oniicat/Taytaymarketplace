73bda01c2dc17446652cfdab0e59e3e1a17e6a56
Jadeeyy <christianjadebumagat@gmail.com>
2024-12-06 22:53:55 +0800

    changeregistertitle

--- admin final na to/admin-registered-shops.php
+++ admin final na to/admin-registered-shops.php
@@ -24,7 +24,7 @@ try {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registered Shops</title>
+    <title>RegisteredShops</title>
     <link rel="stylesheet" href="admin-registered-shops.css">
     <style>
         .modal, .modal-overlay {
